9f3f93d7887f9220af0f8044e33bd4f5529b5555
Григорий Вихрев <ryhorvih@mail.ru>
2023-09-06 03:57:42 +0300

    Добавьте файлы проекта.

---
+++ MyLinkedListLib/LinkedList.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLinkedListLib
+{
+    public class MyLinkedList<T> : ICollection<T>, IEnumerable<T>, IComparable, ICloneable
+    {
+        public Item<T> Head { get; private set; }
+        public Item<T> Tail { get; private set; }
+        public int Count { get; private set; }
+
+        public MyLinkedList()
+        {
+            Head = null;
+            Tail = null;
+            Count = 0;
+
+        }
+
+        public MyLinkedList(T[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+                AddFirst(arr[i]);
+        }
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        public void AddLast(T value)
+        {
+            Item<T> item = new Item<T>(value);
+            AddLast(item);
+        }
+
+        public void AddLast(Item<T> item)
+        {
+            if (Tail == Head && Tail != null)
+            {
+                Tail = item;
+                Head.Next = Tail;
+                Tail.Prev = Head;
+                Tail.Next = null;
+            }
+            else if (Tail == null)
+            {
+                Tail = item;
+                Tail.Next = null;
+                Head = item;
+                Head.Prev = null;
+            }
+            else
+            {
+                Item<T> PreviousLast = Tail;
+                Tail = item;
+                PreviousLast.Next = item;
+                item.Prev = PreviousLast;
+            }
+            Count++;
+        }
+
+        public void Add(T item)
+        {
+            AddLast(item);
+        }
+
+        public void AddFirst(T value)
+        {
+            Item<T> item = new Item<T>(value);
+            AddFirst(item);
+        }
+        public void AddFirst(Item<T> item)
+        {
+            if (Head == Tail && Head != null)
+            {
+                Head = item;
+                Head.Next = Tail;
+                Tail.Prev = Head;
+                Head.Prev = null;
+                Tail.Next = null;
+            }
+            else if (Head == null)
+            {
+                Head = item;
+                Tail = item;
+                Head.Prev = null;
+                Tail.Next = null;
+            }
+            else
+            {
+                Item<T> PreviousFirst = Head;
+                Head = item;
+                PreviousFirst.Prev = item;
+                item.Next = PreviousFirst;
+            }
+            Count++;
+        }
+
+        public void RemoveFirst()
+        {
+            if (Head != null)
+            {
+                Head = Head.Next;
+                Head.Prev = null;
+                Count--;
+            }
+        }
+
+        public void RemoveLast()
+        {
+            if (Tail != null)
+            {
+                Tail = Tail.Prev;
+                Tail.Next = null;
+                Count--;
+            }
+        }
+
+        public void AddBefore(Item<T> item, T value)
+        {
+                Item<T> new_item = new Item<T>(value);
+                new_item.Next = item;
+                item.Prev = new_item;
+                if (Head != item)
+                {
+                    Item<T> old_previous = item.Prev;
+                    old_previous.Next = new_item;
+                    new_item.Prev = old_previous;
+                }
+                else
+                    Head = new_item;
+                Count++;
+        }
+
+        public Item<T> Find(T data)
+        {
+            var item = Head;
+
+            while (item.Next != null)
+            {
+                if (item.Data.Equals(data))
+                {
+                    return item;
+                }
+                else
+                {
+                    item = item.Next;
+                }
+            }
+
+            if (item.Data.Equals(data))
+            {
+                return item;
+            }
+
+            return null;
+        }
+
+        public bool Contains(T data)
+        {
+            var item = Head;
+
+            while (item != null)
+            {
+                if (item.Data.Equals(data))
+                {
+                    return true;
+                }
+                else
+                {
+                    item = item.Next;
+                }
+            }
+
+            return false;
+        }
+
+        public bool Remove(Item<T> item)
+        {
+            if (item != null)
+            {
+                item.Next.Prev = item.Prev;
+                item.Prev.Next = item.Next;
+                Count--;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool Remove(T value)
+        {
+            if (Find(value) == null)
+                return false;
+            else
+            {
+                Remove(Find(value));
+                return true;
+            }
+        }
+
+        public Item<T> FindLast(T data)
+        {
+            var item = Tail;
+
+            while (item.Prev != null)
+            {
+                if (item.Data.Equals(data))
+                {
+                    return item;
+                }
+                else
+                {
+                    item = item.Prev;
+                }
+            }
+
+            if (item.Data.Equals(data))
+            {
+                return item;
+            }
+
+            return null;
+        }
+
+        public void Clear()
+        {
+            Head = null;
+            Tail = null;
+            Count = 0;
+        }
+
+        public void AddAfter(Item<T> item, T value)
+        {
+                Item<T> new_item = new Item<T>(value);
+                new_item.Prev = item;
+                item.Next = new_item;
+                if (Tail != item)
+                {
+                    Item<T> old_next = item.Next;
+                    new_item.Next = old_next;
+                    old_next.Prev = new_item;
+                }
+                else
+                    Tail = new_item;
+                Count++;
+        }
+
+        public object Clone()
+        {
+            MyLinkedList<T> ClonedList = new MyLinkedList<T>();
+            foreach (var value in this)
+            {
+                ClonedList.AddLast(value);
+            }
+            return ClonedList;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null || (obj.GetType() != this.GetType()))
+            {
+                throw new Exception();
+            }
+            MyLinkedList<T> obj1 = obj as MyLinkedList<T>;
+            if (Count < obj1.Count) return -1;
+            if (Count > obj1.Count) return 1;
+            return 0;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            if (Head == null)
+                yield break;
+            Item<T> item = Head;
+            do
+            {
+                yield return item.Data;
+                item = item.Next;
+            } while (item != null);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if (array.Length < arrayIndex + Count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            foreach (var value in this)
+            {
+                array[arrayIndex] = value;
+                arrayIndex++;
+            }
+        }
+
+        public void Sort(IComparer<T> comparer)
+        {
+            T[] array = new T[Count];
+            CopyTo(array, 0);
+            Array.Sort(array, comparer);
+            Item<T> item = Head;
+            for (int i = 0; i < Count; i++)
+            {
+                item.Data = array[i];
+                item = item.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ MyLinkedListTests/Program.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using MyLinkedListLib;
+using System.Diagnostics;
+
+public class Example
+{
+    public static void Main()
+    {
+        Test();
+        Console.ReadLine();
+
+    }
+    public static void Test()
+    {
+        TestLinkedList();
+        TestMyLinkedList();
+
+        //Stopwatch sw = new Stopwatch();
+        //Stopwatch Sw = new Stopwatch();
+
+        //Sw.Start();
+        //for (int i = 0; i < 1; i++) { TestLinkedList(); }
+        //Sw.Stop();
+
+        //sw.Start();
+        //for (int i = 0; i < 1; i++) { TestMyLinkedList(); }
+        //sw.Stop();
+
+        //Console.WriteLine(Sw.ElapsedMilliseconds);
+        //Console.WriteLine(sw.ElapsedMilliseconds);
+    }
+
+    public static void TestLinkedList()
+    {
+        string[] words =
+            { "the", "fox", "jumps", "over", "the", "dog" };
+        LinkedList<string> sentence = new LinkedList<string>(words);
+        Display(sentence, "The linked list values:");
+        Console.WriteLine("sentence.Contains(\"jumps\") = {0}",
+            sentence.Contains("jumps"));
+
+        // Add the word 'today' to the beginning of the linked list.
+        sentence.AddFirst("today");
+        Display(sentence, "Test 1: Add 'today' to beginning of the list:");
+
+        // Move the first node to be the last node.
+        LinkedListNode<string> mark1 = sentence.First;
+        sentence.RemoveFirst();
+        sentence.AddLast(mark1);
+        Display(sentence, "Test 2: Move first node to be last node:");
+
+        // Change the last node to 'yesterday'.
+        sentence.RemoveLast();
+        sentence.AddLast("yesterday");
+        Display(sentence, "Test 3: Change the last node to 'yesterday':");
+
+        // Move the last node to be the first node.
+        mark1 = sentence.Last;
+        sentence.RemoveLast();
+        sentence.AddFirst(mark1);
+        Display(sentence, "Test 4: Move last node to be first node:");
+
+        // Indicate the last occurence of 'the'.
+        sentence.RemoveFirst();
+        LinkedListNode<string> current = sentence.FindLast("the");
+        IndicateNode(current, "Test 5: Indicate last occurence of 'the':");
+
+        // Add 'lazy' and 'old' after 'the' (the LinkedListNode named current).
+        sentence.AddAfter(current, "old");
+        sentence.AddAfter(current, "lazy");
+        IndicateNode(current, "Test 6: Add 'lazy' and 'old' after 'the':");
+
+        // Indicate 'fox' node.
+        current = sentence.Find("fox");
+        IndicateNode(current, "Test 7: Indicate the 'fox' node:");
+
+        // Add 'quick' and 'brown' before 'fox':
+        sentence.AddBefore(current, "quick");
+        sentence.AddBefore(current, "brown");
+        IndicateNode(current, "Test 8: Add 'quick' and 'brown' before 'fox':");
+
+        // Keep a reference to the current node, 'fox',
+        // and to the previous node in the list. Indicate the 'dog' node.
+        mark1 = current;
+        LinkedListNode<string> mark2 = current.Previous;
+        current = sentence.Find("dog");
+        IndicateNode(current, "Test 9: Indicate the 'dog' node:");
+
+        // Remove the node referred to by current.
+        sentence.Remove(current);
+        IndicateNode(current, "Test 12: Remove current node (dog) and attempt to indicate it:");
+
+        // The Remove method finds and removes the
+        // first node that that has the specified value.
+        sentence.Remove("old");
+        Display(sentence, "Test 14: Remove node that has the value 'old':");
+
+        // When the linked list is cast to ICollection(Of String),
+        // the Add method adds a node to the end of the list.
+        sentence.RemoveLast();
+        ICollection<string> icoll = sentence;
+        icoll.Add("rhinoceros");
+        Display(sentence, "Test 15: Remove last node, cast to ICollection, and add 'rhinoceros':");
+
+        Console.WriteLine("Test 16: Copy the list to an array:");
+        // Create an array with the same number of
+        // elements as the linked list.
+        string[] sArray = new string[sentence.Count];
+        sentence.CopyTo(sArray, 0);
+
+        foreach (string s in sArray)
+        {
+            Console.WriteLine(s);
+        }
+
+        // Release all the nodes.
+        sentence.Clear();
+
+        Console.WriteLine();
+        Console.WriteLine("Test 17: Clear linked list. Contains 'jumps' = {0}",
+            sentence.Contains("jumps"));
+    }
+
+    private static void Display(LinkedList<string> words, string test)
+    {
+        Console.WriteLine(test);
+        foreach (string word in words)
+        {
+            Console.Write(word + " ");
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+    }
+
+    private static void IndicateNode(LinkedListNode<string> node, string test)
+    {
+        Console.WriteLine(test);
+        if (node.List == null)
+        {
+            Console.WriteLine("Node '{0}' is not in the list.\n",
+                node.Value);
+            return;
+        }
+
+        StringBuilder result = new StringBuilder("(" + node.Value + ")");
+        LinkedListNode<string> nodeP = node.Previous;
+
+        while (nodeP != null)
+        {
+            result.Insert(0, nodeP.Value + " ");
+            nodeP = nodeP.Previous;
+        }
+
+        node = node.Next;
+        while (node != null)
+        {
+            result.Append(" " + node.Value);
+            node = node.Next;
+        }
+
+        Console.WriteLine(result);
+        Console.WriteLine();
+    }
+
+    public static void TestMyLinkedList()
+    {
+        string[] words =
+            { "the", "fox", "jumps", "over", "the", "dog" };
+        MyLinkedList<string> sentence = new MyLinkedList<string>(words);
+        Display(sentence, "The linked list values:");
+        Console.WriteLine("sentence.Contains(\"jumps\") = {0}",
+            sentence.Contains("jumps"));
+
+        // Add the word 'today' to the beginning of the linked list.
+        sentence.AddFirst("today");
+        Display(sentence, "Test 1: Add 'today' to beginning of the list:");
+
+        // Move the first node to be the last node.
+        Item<string> mark1 = sentence.Head;
+        sentence.RemoveFirst();
+        sentence.AddLast(mark1);
+        Display(sentence, "Test 2: Move first node to be last node:"); /////
+
+        // Change the last node to 'yesterday'.
+        sentence.RemoveLast();
+        sentence.AddLast("yesterday");
+        Display(sentence, "Test 3: Change the last node to 'yesterday':");
+
+        // Move the last node to be the first node.
+        mark1 = sentence.Tail;
+        sentence.RemoveLast();
+        sentence.AddFirst(mark1);
+        Display(sentence, "Test 4: Move last node to be first node:");
+
+        // Indicate the last occurence of 'the'.
+        sentence.RemoveFirst();
+        Item<string> current = sentence.FindLast("the");
+        IndicateNode(current, "Test 5: Indicate last occurence of 'the':");
+
+        // Add 'lazy' and 'old' after 'the' (the LinkedListNode named current).
+        sentence.AddAfter(current, "old");
+        sentence.AddAfter(current, "lazy");
+        IndicateNode(current, "Test 6: Add 'lazy' and 'old' after 'the':");
+
+        // Indicate 'fox' node.
+        current = sentence.Find("fox");
+        IndicateNode(current, "Test 7: Indicate the 'fox' node:");
+
+        // Add 'quick' and 'brown' before 'fox':
+        sentence.AddBefore(current, "quick");
+        sentence.AddBefore(current, "brown");
+        IndicateNode(current, "Test 8: Add 'quick' and 'brown' before 'fox':");
+
+        // Keep a reference to the current node, 'fox',
+        // and to the previous node in the list. Indicate the 'dog' node.
+        mark1 = current;
+        Item<string> mark2 = current.Prev;
+        current = sentence.Find("dog");
+        IndicateNode(current, "Test 9: Indicate the 'dog' node:");
+
+        // Remove the node referred to by current.
+        sentence.Remove(current);
+        IndicateNode(current, "Test 12: Remove current node (dog) and attempt to indicate it:");
+
+        // The Remove method finds and removes the
+        // first node that that has the specified value.
+        sentence.Remove("old");
+        Display(sentence, "Test 14: Remove node that has the value 'old':");
+
+        // When the linked list is cast to ICollection(Of String),
+        // the Add method adds a node to the end of the list.
+        sentence.RemoveLast();
+        ICollection<string> icoll = sentence;
+        icoll.Add("rhinoceros");
+        Display(sentence, "Test 15: Remove last node, cast to ICollection, and add 'rhinoceros':");
+
+        Console.WriteLine("Test 16: Copy the list to an array:");
+        // Create an array with the same number of
+        // elements as the linked list.
+        string[] sArray = new string[sentence.Count];
+        sentence.CopyTo(sArray, 0);
+
+        foreach (string s in sArray)
+        {
+            Console.WriteLine(s);
+        }
+
+        // Release all the nodes.
+        sentence.Clear();
+
+        Console.WriteLine();
+        Console.WriteLine("Test 17: Clear linked list. Contains 'jumps' = {0}",
+            sentence.Contains("jumps"));
+    }
+
+    private static void Display(MyLinkedList<string> words, string test)
+    {
+        Console.WriteLine(test);
+        foreach (string word in words)
+        {
+            Console.Write(word + " ");
+        };
+        Console.WriteLine();
+        Console.WriteLine();
+    }
+
+    private static void IndicateNode(Item<string> node, string test)
+    {
+        StringBuilder result = new StringBuilder("(" + node.Data + ")");
+        Item<string> nodeP = node.Prev;
+
+        while (nodeP != null)
+        {
+            result.Insert(0, nodeP.Data + " ");
+            nodeP = nodeP.Prev;
+        }
+
+        node = node.Next;
+        while (node != null)
+        {
+            result.Append(" " + node.Data);
+            node = node.Next;
+        }
+
+        Console.WriteLine(result);
+        Console.WriteLine();
+    }
+}
